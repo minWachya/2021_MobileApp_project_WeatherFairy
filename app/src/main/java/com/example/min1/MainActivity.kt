5e62268ee99eb3ef6adba1084e711af15d003e62
minWachya <alal7272@gmail.com>
2021-05-15 15:49:51 +0900

    add recommend clouths

--- app/src/main/java/com/example/min1/MainActivity.kt
+++ app/src/main/java/com/example/min1/MainActivity.kt
@@ -47,6 +47,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var tvHumidity : TextView      // 습도
     lateinit var tvSky : TextView           // 하늘 상태
     lateinit var tvTemp : TextView          // 온도
+    lateinit var tvRecommend : TextView     // 기본 옷 추천
     lateinit var btnWrite : Button          // <옷 기록하기> 버튼
     lateinit var btnSeeMemo : Button        // <내 기록보기> 버튼
 
@@ -65,6 +66,7 @@ class MainActivity : AppCompatActivity() {
         tvHumidity = findViewById(R.id.tvHumidity)
         tvSky = findViewById(R.id.tvSky)
         tvTemp = findViewById(R.id.tvTemp)
+        tvRecommend = findViewById(R.id.tvRecommend)
         btnWrite = findViewById(R.id.btnWrite)
         btnSeeMemo = findViewById(R.id.btnSeeMemo)
 
@@ -108,24 +110,24 @@ class MainActivity : AppCompatActivity() {
                     // 날씨 정보 가져오기
                     var it: List<ITEM> = response.body()!!.response.body.items.item
 
-                    var rainRatio = ""      // 강수 확률
-                    var rainType = ""       // 강수 형태
+                    var temp = ""           // 기온
                     var humidity = ""       // 습도
                     var sky = ""            // 하능 상태
-                    var temp = ""           // 기온
+                    var rainRatio = ""      // 강수 확률
+                    var rainType = ""       // 강수 형태
                     for (i in 0..9) {
                         when(it[i].category) {
-                            "POP" -> rainRatio = it[i].fcstValue    // 강수 기온
-                            "PTY" -> rainType = it[i].fcstValue     // 강수 형태
+                            "T3H" -> temp = it[i].fcstValue         // 기온
                             "REH" -> humidity = it[i].fcstValue     // 습도
                             "SKY" -> sky = it[i].fcstValue          // 하늘 상태
-                            "T3H" -> temp = it[i].fcstValue         // 기온
+                            "POP" -> rainRatio = it[i].fcstValue    // 강수 기온
+                            "PTY" -> rainType = it[i].fcstValue     // 강수 형태
                             else -> continue
                         }
 
                     }
                     // 날씨 정보 텍스트뷰에 보이게 하기
-                    setWeather(rainRatio, rainType, humidity, sky, temp)
+                    setWeather(temp, humidity, sky, rainRatio, rainType)
 
                     // 토스트 띄우기
                     Toast.makeText(applicationContext, it[0].fcstDate + ", " + it[0].fcstTime + "의 날씨 정보입니다.", Toast.LENGTH_SHORT).show()
@@ -140,11 +142,24 @@ class MainActivity : AppCompatActivity() {
     }
 
     // 텍스트 뷰에 날씨 정보 보여주기
-    fun setWeather(rainRatio : String, rainType : String, humidity : String, sky : String, temp : String) {
+    fun setWeather(temp : String, humidity : String, sky : String, rainRatio : String, rainType : String) {
+        // 온도
+        tvTemp.text = temp + "°"
+        // 습도
+        tvHumidity.text = humidity + "%"
+        // 하능 상태
+        var result = ""
+        when(sky) {
+            "1" -> result = "맑음"
+            "3" -> result = "구름 많음"
+            "4" -> result = "흐림"
+            else -> "오류"
+        }
+        tvSky.text = result
         // 강수 확률
         tvRainRatio.text = rainRatio + "%"
         // 강수 형태
-        var result = ""
+        result = ""
         when(rainType) {
             "0" -> result = "없음"
             "1" -> result = "비"
@@ -157,19 +172,18 @@ class MainActivity : AppCompatActivity() {
             else -> "오류"
         }
         tvRainType.text = result
-        // 습도
-        tvHumidity.text = humidity + "%"
-        // 하능 상태
-        result = ""
-        when(sky) {
-            "1" -> result = "맑음"
-            "3" -> result = "구름 많음"
-            "4" -> result = "흐림"
-            else -> "오류"
+        // 기본 옷 추천
+        when (temp) {
+            in "5".."8" -> result = "울 코트, 가죽 옷, 기모"
+            in "9".."11" -> result = "트렌치 코트, 야상, 점퍼"
+            in "12".."16" -> result = "자켓, 가디건, 청자켓"
+            in "17".."19" -> result = "니트, 맨투맹, 후드, 긴바지"
+            in "20".."22" -> result = "블라우스, 긴팔 티, 슬랙스"
+            in "23".."27" -> result = "얇은 셔츠, 반바지, 면바지"
+            in "28".."50" -> result = "민소매, 반바지, 린넨 옷"
+            else -> result = "패딩, 누빔 옷, 목도리"
         }
-        tvSky.text = result
-        // 온도
-        tvTemp.text = temp + "°"
+        tvRecommend.text = result
     }
 
     // 시간 설정하기
