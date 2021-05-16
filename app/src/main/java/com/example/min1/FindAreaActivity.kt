766a77caa5cfa776db4a4dc8da703974f4878808
minWachya <alal7272@gmail.com>
2021-05-16 12:27:21 +0900

    add marker name

--- app/src/main/java/com/example/min1/FindAreaActivity.kt
+++ app/src/main/java/com/example/min1/FindAreaActivity.kt
@@ -1,5 +1,6 @@
 package com.example.min1
 
+import android.app.Activity
 import android.os.Bundle
 import android.util.Log
 import android.view.ViewGroup
@@ -7,13 +8,17 @@ import android.widget.Button
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import net.daum.mf.map.api.MapPOIItem
+import net.daum.mf.map.api.MapPoint
 import net.daum.mf.map.api.MapReverseGeoCoder
 import net.daum.mf.map.api.MapView
-import net.daum.mf.map.api.MapPoint as MapPoint
 
-class FindAreaActivity  : AppCompatActivity() {
+class FindAreaActivity  : AppCompatActivity(), MapReverseGeoCoder.ReverseGeoCodingResultListener {
     lateinit var btnBack1 : Button
     lateinit var map : LinearLayout
+    lateinit var marker : MapPOIItem
+    lateinit var mapView : MapView
+
+    var areaName = "지역명"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,38 +28,44 @@ class FindAreaActivity  : AppCompatActivity() {
         map = findViewById(R.id.map)
 
         // 카카오맵
-        val mapView = MapView(this)
+        mapView = MapView(this)
         val mapViewContainer = map as ViewGroup
         mapViewContainer.addView(mapView)
 
-        //var maker = MapPOIItem()
-        //var area = MapPoint.mapPointWithGeoCoord(37.541889,127.095388)
-        //area.mapPointGeoCoord       // 위경도 나타내기
         ////var areaName = MapReverseGeoCoder.findAddressForMapPoint("", area)
-
-        ////Log.d("mmm 좌표", maker.mapPoint.toString())
         ////Log.d("mmm 지역명", areaName)
-        //maker.markerType = MapPOIItem.MarkerType.BluePin    // 마커 타입 설정
-
-        //mapView.setShowCurrentLocationMarker(true)  // 마커 보이기
-        //mapView.setDefaultCurrentLocationMarker()   // 기본 제공되는 현위치 아이콘
-        ////mapView.setMapCenterPoint()               // 지도 화면의 중심점 설정
-
-        ////var area = MapPoint.mapPointWithCONGCoord(double, double)   // 해당 위경도 좌표의 객체 반환
-
-
-        val marker = MapPOIItem()
-        marker.itemName = "마커위에 나올 이름"
+        marker = MapPOIItem()
+        marker.itemName = areaName
         marker.tag = 0
-        marker.mapPoint = MapPoint.mapPointWithGeoCoord(37.68741320960496, 126.77534893056148)
-        marker.markerType = MapPOIItem.MarkerType.BluePin
-        marker.selectedMarkerType = MapPOIItem.MarkerType.RedPin
+        marker.mapPoint = MapPoint.mapPointWithGeoCoord(37.65136866943945, 127.01617112670128)
+        marker.markerType = MapPOIItem.MarkerType.BluePin   // 마커 타입 설정
         mapView.addPOIItem(marker)
         mapView.setMapCenterPoint(marker.mapPoint, true)               // 지도 화면의 중심점 설정
 
-        
+        val reverseGeoCoder = MapReverseGeoCoder(
+            "869b0ecf65dacae7d89ac1bba906e8cf",
+            marker.mapPoint,
+            this,
+            this
+        )
+        reverseGeoCoder.startFindingAddress()
+
         btnBack1.setOnClickListener {
             finish()
         }
     }
+
+    // ReverseGeoCodingResultListener 재정의
+    // 주소 찾기 성공
+    override fun onReverseGeoCoderFoundAddress(p0: MapReverseGeoCoder?, p1: String?) {
+        Log.d("mmm 주소 성공", p1!!)
+        marker.itemName = p1!!
+        mapView.addPOIItem(marker)
+
+    }
+    // 주소 찾기 실패
+    override fun onReverseGeoCoderFailedToFindAddress(p0: MapReverseGeoCoder?) {
+        marker.itemName = "주소를 찾지 못하였습니다."
+    }
+
 }
