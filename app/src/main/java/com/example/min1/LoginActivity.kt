4caa4f8388ff44dd65c344958db7751acb38a4a6
minWachya <alal7272@gmail.com>
2021-05-17 17:33:45 +0900

    success naver login

--- app/src/main/java/com/example/min1/LoginActivity.kt
+++ app/src/main/java/com/example/min1/LoginActivity.kt
@@ -1,19 +1,28 @@
 package com.example.min1
 
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.user.UserApiClient
+import com.nhn.android.naverlogin.OAuthLogin
+import com.nhn.android.naverlogin.OAuthLoginHandler
+import com.nhn.android.naverlogin.ui.view.OAuthLoginButton
 
 // 로그인 액티비티(시작 화면)
 class LoginActivity : AppCompatActivity() {
-    lateinit var btnKakaoLogin : ImageButton
-    var TAG = "kakaoLogin"
+    lateinit var btnKakaoLogin : ImageButton        // 카카오 로그인
+    lateinit var btnNaverLogin : OAuthLoginButton   // 네이버 로그인
+    lateinit var mOAuthLoginInstance : OAuthLogin
+    lateinit var mContext: Context
+
+    var TAG = "Login"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,16 +30,17 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         btnKakaoLogin = findViewById(R.id.btnKakaoLogin)
+        btnNaverLogin = findViewById(R.id.btnNaverLogin)
 
         // 카카오 디벨로퍼 홈페이지의 "카카오 로그인 구현 예제" 복붙 후 수정
         // 로그인 공통 callback 구성
         val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
             if (error != null) {
-                Log.e(TAG, "로그인 실패", error)
+                Log.e(TAG, "카카오 로그인 실패", error)
             }
             // 로그인 성공하면 다음 화면 보이기
             else if (token != null) {
-                Log.i(TAG, "로그인 성공 ${token.accessToken}")
+                Log.i(TAG, "카카오 로그인 성공 ${token.accessToken}")
 
                 // MainActivity로 넘어가기
                 var intent = Intent(this@LoginActivity, MainActivity::class.java)
@@ -49,5 +59,32 @@ class LoginActivity : AppCompatActivity() {
             }
         }
 
+
+        //  네이버 아이디로 로그인
+        val naver_client_id = "KHicI1oFhj5zdt5VGjma"
+        val naver_client_secret = "oBclxogSES"
+        val naver_client_name = "끼양"
+
+        mContext = this@LoginActivity
+        mOAuthLoginInstance = OAuthLogin.getInstance()
+        mOAuthLoginInstance.init(mContext, naver_client_id, naver_client_secret, naver_client_name)
+        btnNaverLogin.setOAuthLoginHandler(mOAuthLoginHandler)
     }
+
+    val mOAuthLoginHandler: OAuthLoginHandler = object : OAuthLoginHandler() {
+        override fun run(success: Boolean) {
+            if (success) {
+                Log.d(TAG, "네이버 로그인 성공")
+
+                // MainActivity로 넘어가기
+                var intent = Intent(this@LoginActivity, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                Log.i(TAG, "네이버 로그인 실패")
+            }
+        }
+
+    }
+
 }
